bf55bf95fec8cb2c4538c964c229478ae8993c4d
MetiBluer <133645933+MetiBluer@users.noreply.github.com>
2024-07-08 12:05:26 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Code_Tolentino_Francesz_Inv
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtProdID = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnPurchh = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.btnRecord = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtProdID
+            // 
+            this.txtProdID.Location = new System.Drawing.Point(62, 33);
+            this.txtProdID.Name = "txtProdID";
+            this.txtProdID.Size = new System.Drawing.Size(100, 20);
+            this.txtProdID.TabIndex = 0;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(185, 33);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 1;
+            // 
+            // btnPurchh
+            // 
+            this.btnPurchh.Location = new System.Drawing.Point(62, 73);
+            this.btnPurchh.Name = "btnPurchh";
+            this.btnPurchh.Size = new System.Drawing.Size(75, 23);
+            this.btnPurchh.TabIndex = 2;
+            this.btnPurchh.Text = "Purchase";
+            this.btnPurchh.UseVisualStyleBackColor = true;
+            this.btnPurchh.Click += new System.EventHandler(this.btnPurchh_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(185, 73);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(53, 120);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.Size = new System.Drawing.Size(475, 211);
+            this.dgvProducts.TabIndex = 4;
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(53, 350);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 5;
+            this.btnRecord.Text = "Open Form";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 395);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnPurchh);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtProdID);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProdID;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnPurchh;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Button btnRecord;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Code_Tolentino_Francesz_Inv
+{
+    public partial class frmMain : Form
+    {
+        OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Z:\\169_Tolentino_Francesz_SC\\dbSchoolSupplies.accdb");
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            grid();
+        }
+
+        private void btnPurchh_Click(object sender, EventArgs e)
+        {
+
+        }
+        void grid()
+        {
+            String query = "SELECT * FROM product order by prodID asc";
+            OleDbCommand cmd = new OleDbCommand(query, conn);
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvProducts.DataSource = dt;
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            frmRecordMgmt frm = new frmRecordMgmt();
+            frm.ShowDialog();
+        }
+    }
+}
